cb8b46d881384dc1fa52efe9d611f33db5b31408
XeuWayy <corentin.gafam@proton.me>
2026-02-07 15:13:37 +0100

    Add ✨: Parse & Validation of the Argv input.

---
+++ clients/AI/Main.js
@@ -0,0 +1,62 @@
+import process from 'node:process'
+import {ARGV_ERROR, DEFAULT_HOSTNAME} from "./constant.js";
+
+class Main {
+    constructor() {
+        this.config = {
+            teamName: null,
+            port: null,
+            hostname: DEFAULT_HOSTNAME,
+        }
+
+        const argv = process.argv.slice(2);
+
+        try {
+            this.parseArgv(argv);
+            console.log('current config :', this.config)
+        } catch (error) {
+            console.error(error.message);
+            process.exit(1);
+        }
+    }
+
+    /**
+     * @author Corentin (ccharton) Charton
+     * @description Check validity of input argument will throw an Error on failure
+     * @param argv {String[]} - The argument list to parse
+     */
+    parseArgv(argv) {
+        if (argv.length !== 4 && argv.length !== 6) {
+            throw Error(ARGV_ERROR);
+        }
+
+        if (argv[0] !== '-n' || argv[2] !== '-p' || argv[4] && argv[4] !== '-h') {
+            throw Error(ARGV_ERROR)
+        }
+
+        this.config.teamName = argv[1]
+        this.config.port = this.parsePort(argv[3])
+        this.config.hostname = argv[5] || DEFAULT_HOSTNAME
+    }
+
+    /**
+     * author Corentin (ccharton) Charton
+     * @param portStr {String} - The string Port to parse and convert to a Number(Int)
+     * @returns {number} - The parsed port in Int.
+     */
+    parsePort(portStr) {
+        if (!/^\d+$/.test(portStr)) {
+            throw new Error(`Port "${portStr}" is not a valid Int.\n\n${ARGV_ERROR}`);
+        }
+
+        const port = parseInt(portStr);
+
+        if (port < 1 || port > 65535) {
+            throw new Error(`Port ${port} is not in range (1-65535).\n\n${ARGV_ERROR}`);
+        }
+
+        return port;
+    }
+}
+
+new Main();
---
+++ clients/AI/constant.js
@@ -0,0 +1,8 @@
+export const DEFAULT_HOSTNAME = 'localhost';
+
+
+/*****************
+ *     ERROR     *
+ ****************/
+
+export const ARGV_ERROR = 'Usage: ./client -n <team> -p <port> [-h <hostname>]\n-n team_name\n-p port\n-h name of the host, by default localhost';
