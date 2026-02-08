1a9dda2973190d0e416601a8a079c7e25a5e9999
XeuWayy <corentin.gafam@proton.me>
2026-02-08 16:40:59 +0100

    Add ✨: Object, Server message constant is defined.

--- clients/AI/constant.js
+++ clients/AI/constant.js
@@ -6,3 +6,52 @@ export const DEFAULT_HOSTNAME = 'localhost';
  ****************/
 
 export const ARGV_ERROR = 'Usage: ./client -n <team> -p <port> [-h <hostname>]\n-n team_name\n-p port\n-h name of the host, by default localhost';
+
+
+/*****************
+ *     OBJECT    *
+ ****************/
+
+export const FOOD = 'nourriture'
+export const LINEMATE = 'linemate'
+export const DERAUMERE = 'deraumere'
+export const SIBUR = 'sibur'
+export const PHIRAS = 'phiras'
+export const THISTAME = 'thistame'
+
+
+/*****************
+ *  SERVER MSG   *
+ ****************/
+
+export const ADVANCE = 'avance\n'
+export const RIGHT = 'droite\n'
+export const LEFT = 'gauche\n'
+export const SEE = 'voir\n'
+export const INVENTORY = 'inventaire\n'
+
+export const TAKE_FOOD = `prend ${FOOD}\n`
+export const TAKE_LINEMATE = `prend ${LINEMATE}\n`
+export const TAKE_DERAUMERE = `prend ${DERAUMERE}\n`
+export const TAKE_SIBUR = `prend ${SIBUR}\n`
+export const TAKE_PHIRAS = `prend ${PHIRAS}\n`
+export const TAKE_THISTAME = `prend ${THISTAME}\n`
+
+export const PUT_FOOD = `pose ${FOOD}\n`
+export const PUT_LINEMATE = `pose ${LINEMATE}\n`
+export const PUT_DERAUMERE = `pose ${DERAUMERE}\n`
+export const PUT_SIBUR = `pose ${SIBUR}\n`
+export const PUT_PHIRAS = `pose ${PHIRAS}\n`
+export const PUT_THISTAME = `pose ${THISTAME}\n`
+
+export const KICK = 'expulse\n'
+export const INCANTATION = 'incatation\n'
+export const FORK = 'fork\n'
+export const AVAILABLE_CONNECTION = 'connect_nbr\n'
+
+
+/*****************
+ * BROADCAST MSG *
+ ****************/
+// TODO: DEFINE AVAILABLE BROADCAST MESSAGE
+export const BROADCAST = 'broadcast'
