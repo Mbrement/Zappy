f232acce4da003a7cb813655a01e2b366f8cbbeb
Emmatosorus <emma@bouteyres.com>
2026-02-12 14:56:18 +0100

    Dev 👨‍💻: unlocked electron window size for development

--- clients/gfx/src/electron.js
+++ clients/gfx/src/electron.js
@@ -2,10 +2,13 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        minWidth: 1200,
-        minHeight: 800,
-        maxWidth: 1200,
-        maxHeight: 800,
+        width: 1200,
+        height: 800,
+        // [TEMPORARY]
+        // minWidth: 1200,
+        // minHeight: 800,
+        // maxWidth: 1200,
+        // maxHeight: 800,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
