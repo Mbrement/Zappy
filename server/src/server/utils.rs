c305eed70c9a6db7eeb33b72e4e687cfa0fde917
M-ROZNIECKI <blueyesmax69@gmail.com>
2026-02-13 10:32:47 +0100

    Fix fn debug_manager_register

--- server/src/server/utils.rs
+++ server/src/server/utils.rs
@@ -1,12 +1,13 @@
 use crate::server::Server;
+use std::io::Write;
 
-fn debug_manager_register(s: String, option: bool) {
-    #[cfg(feature = "debug")]
+fn debug_manager_register(s: String, _c: mio::Token, server: &mut Server, _arg: &str) {
+    //#[cfg(feature = "debug")]
     let client = server._clients.get_mut(&_c).unwrap();
-    #[cfg(feature = "debug")]
+    //#[cfg(feature = "debug")]
     let _ = client
         .get_socket_mut()
         .write(format!("command {} recived {{{}}}\n", s, _arg).as_bytes());
-	#[cfg(feature = "debug")]
+	//#[cfg(feature = "debug")]
     println!("command {} recived {{{}}} {:?}", s, _arg, _c);
 }
