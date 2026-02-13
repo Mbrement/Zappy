e4e0804997c1fe45940f220e491f23450e77fe5a
M-ROZNIECKI <blueyesmax69@gmail.com>
2026-02-13 10:33:53 +0100

    removed comment

--- server/src/server/utils.rs
+++ server/src/server/utils.rs
@@ -2,12 +2,12 @@ use crate::server::Server;
 use std::io::Write;
 
 fn debug_manager_register(s: String, _c: mio::Token, server: &mut Server, _arg: &str) {
-    //#[cfg(feature = "debug")]
+    #[cfg(feature = "debug")]
     let client = server._clients.get_mut(&_c).unwrap();
-    //#[cfg(feature = "debug")]
+    #[cfg(feature = "debug")]
     let _ = client
         .get_socket_mut()
         .write(format!("command {} recived {{{}}}\n", s, _arg).as_bytes());
-	//#[cfg(feature = "debug")]
+	#[cfg(feature = "debug")]
     println!("command {} recived {{{}}} {:?}", s, _arg, _c);
 }
