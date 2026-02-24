c25873dff2e4f5fccda173fa18a80157b74326d6
XeuWayy <corentin.gafam@proton.me>
2026-02-24 13:32:53 +0100

    Fix 🧑‍🔧: Missing refresh rate constant.

--- clients/AI/constant.js
+++ clients/AI/constant.js
@@ -1,5 +1,7 @@
 export const DEFAULT_HOSTNAME = 'localhost';
 export const MAX_SERVER_MSG = 10
+export const VISION_REFRESH_RATE = 500
+export const INVENTORY_REFRESH_RATE = 5000
 
 /*****************
  *     ERROR     *
