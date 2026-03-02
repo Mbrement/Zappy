10b5d3d02adc303ff745476c6f603be6fb61a0ef
XeuWayy <corentin.gafam@proton.me>
2026-03-02 16:00:17 +0100

    Fix ✨: GFX launch script not working on other OS/uArch than Linux x64.

--- clients/gfx/scripts/runGFX.sh
+++ clients/gfx/scripts/runGFX.sh
@@ -1,12 +1,38 @@
-cd $(dirname "$0")
+#!/bin/bash
+cd "$(dirname "$0")"
 
-export EXECUTABLE="../out/gfx-linux-x64/gfx"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  PLATFORM='linux'
+  BINARY_NAME='gfx'
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  PLATFORM='darwin'
+  BINARY_NAME='gfx.app'
+else
+  echo "Unsupported OS: $OSTYPE"
+  exit 1
+fi
 
-if [ -f "$EXECUTABLE" ]; then
-  chmod +x $EXECUTABLE
-  ./$EXECUTABLE
+UNAME_ARCH=$(uname -m)
+
+if [[ "$UNAME_ARCH" == "x86_64" ]]; then
+  ELECTRON_ARCH="x64"
+elif [[ "$UNAME_ARCH" == "aarch64" || "$UNAME_ARCH" == "arm64" ]]; then
+  ELECTRON_ARCH="arm64"
+else
+  echo "Unsupported Uarch: $UNAME_ARCH"
+  exit 1
+fi
+
+EXECUTABLE_DIR="../out/gfx-${PLATFORM}-${ELECTRON_ARCH}/"
+
+if [ -d "$EXECUTABLE_DIR" ]; then
+  cd "$EXECUTABLE_DIR" || exit
+  
+  if [[ "$PLATFORM" == "linux" ]]; then
+    ./"$BINARY_NAME"
+  elif [[ "$PLATFORM" == "darwin" ]]; then
+    open "$BINARY_NAME"
+  fi
 else
   echo "Zappy GFX executable not found, please make sure you ran make build_gfx"
 fi
-
-unset EXECUTABLE
